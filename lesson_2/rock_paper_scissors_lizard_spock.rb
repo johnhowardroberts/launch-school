7b50d0064eaf47cbd4d3e52c01224c115345ce66
John Roberts <jhr.roberts@gmail.com>
2018-11-22 19:38:17 -0800

    updated RPSLS file

--- lesson_2/rock_paper_scissors_lizard_spock.rb
+++ lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -8,14 +8,14 @@ WINNING_CHOICES = {
   'lizard' =>   ['paper', 'spock']
 }
 
-WINNING_SCORE = 5
+WINNING_SCORE = 1
 
 def clear_screen
   system('clear') || system('cls')
 end
 
 def line_break
-  Kernel.puts("===============================")
+  puts("===============================")
 end
 
 def load_delay_short
@@ -26,8 +26,8 @@ def load_delay_long
   sleep(1.5)
 end
 
-def welcome_message
-  puts "Welcome to Rock Paper Scissors Lizard Spock! First to 5 wins."
+def display_welcome_message
+  puts "Welcome to Rock Paper Scissors Lizard Spock! First to #{WINNING_SCORE} wins."
 end
 
 def print_loading_next_round
@@ -46,15 +46,36 @@ def win?(first, second)
   WINNING_CHOICES[first].include?(second)
 end
 
+# def play_again?
+#   answer = ''
+#   loop do
+#     answer = gets.chomp.downcase
+#     if answer = 'y'
+#       return true
+#     elsif answer = 'n'
+#       return false
+#     else
+#       prompt('Invalid choice. Do you want to play again? (y/n)')
+#     end
+#   end
+# end
+
 player_score = 0
 computer_score = 0
 round_number = 1
 
 line_break
-welcome_message
+display_welcome_message
 line_break
 prompt("What's your name?")
-player_name = gets.chomp
+player_name = gets.chomp.to_s
+
+loop do
+  break unless player_name.empty?
+  prompt("I'm sorry, I didn't quite get that. Please enter your name.")
+  player_name = gets.chomp.to_s
+end
+
 prompt("Welcome to the game #{player_name}!")
 
 loop do
@@ -64,27 +85,27 @@ loop do
     round_number += 1
     prompt("Choose one: rock (r), paper (p), scissors (s), lizard (l) or \
 spock (sp).")
-    player_choice = Kernel.gets().downcase().chomp()
-
-    case player_choice
-    when 'r'
-      player_choice = 'rock'
-    when 'p'
-      player_choice = 'paper'
-    when 's'
-      player_choice = 'scissors'
-    when 'l'
-      player_choice = 'lizard'
-    when 'sp'
-      player_choice = 'spock'
-    end
+    player_choice = gets.downcase.chomp
 
     loop do
+      case player_choice
+      when 'r'
+        player_choice = 'rock'
+      when 'p'
+        player_choice = 'paper'
+      when 's'
+        player_choice = 'scissors'
+      when 'l'
+        player_choice = 'lizard'
+      when 'sp'
+        player_choice = 'spock'
+      end
+
       break if VALID_CHOICES.include?(player_choice)
 
       prompt("That's not a valid choice. Choose one: rock (r), paper (p), \
 scissors (s), lizard (l) or spock (sp).")
-      player_choice = Kernel.gets().downcase().chomp()
+      player_choice = gets.downcase.chomp
     end
 
     computer_choice = VALID_CHOICES.sample
@@ -129,8 +150,15 @@ scissors (s), lizard (l) or spock (sp).")
   player_score = 0
   computer_score = 0
   round_number = 1
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer = gets.chomp
+
+  loop do
+    break if answer.downcase == 'y' || answer.downcase == 'n'
+    prompt('Please enter a valid choice (y/n)')
+    answer = gets.chomp
+  end
+
+  break unless answer.downcase == 'y'
   clear_screen
   prompt("NEW GAME!")
 end
